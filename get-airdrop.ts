ba9d86b44ace3c2676f20d24f43e8f18ebb1035e
Harshad Goswami <harshad.goswami@gmail.com>
2024-07-12 21:51:04 +0530

    tron

---
+++ get-airdrop.ts
@@ -0,0 +1,40 @@
+import axios from 'axios';
+import { TronWeb } from 'tronweb';
+
+
+const tronWeb = new TronWeb({
+    fullHost: 'https://api.shasta.trongrid.io',
+    // You can also use other nodes for mainnet or different testnets
+});
+
+// Create a new account
+async function createAccount() {
+    const account = await tronWeb.createAccount();
+    console.log('New Account Address:', account.address.base58);
+    console.log('Private Key:', account.privateKey);
+    return account;
+}
+
+// Request an airdrop from Tron faucet
+async function requestAirdrop(address: string) {
+    try {
+        const response = await axios.post('https://api.shasta.trongrid.io/wallet/faucet', {
+            address
+        });
+        console.log('Airdrop Response:', response.data);
+    } catch (error) {
+        console.error('Error requesting airdrop:', error);
+    }
+}
+
+async function main() {
+    // Create a new account
+    const account = await createAccount();
+
+    // Request an airdrop for the new account
+    await requestAirdrop(account.address.base58);
+}
+
+main().catch((err) => {
+    console.error(err);
+});
---
+++ tron.md
@@ -0,0 +1,5 @@
+### Tron document refenrece
+
+- [tron faucet](https://shasta.tronex.io/)
+- [tron network links](https://developers.tron.network/docs/networks)
+- [tron api documentation](https://developers.tron.network/reference/background)
